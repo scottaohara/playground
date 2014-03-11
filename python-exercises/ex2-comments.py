0c467b34a0ee6ca360d55077d890ede6c2149774
Scott O'Hara <scottaohara@gmail.com>
2014-03-11 13:09:58 -0400

    remove python exercises

--- python-exercises/ex2-comments.py
+++
@@ -1,9 +0,0 @@
-# making comments like it's my job
-# because right now it actually sorta is...
-
-print "this is a line of code to be printed " # comments go after the line
-
-# comments are used to disable lines of code.
-# print "this will not actually run, cause it\'s commented out."
-
-print "this will run.  ###  and i\'ll keep practicing back slashes for single quotes."
