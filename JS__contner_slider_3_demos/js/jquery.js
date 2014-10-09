d49cc13e0053b96de2cfa51c334b38f3bc458e4f
Scott <scottaohara@gmail.com>
2014-10-09 09:07:44 -0400

    move javascript into individual files

---
+++ JS__contner_slider_3_demos/js/jquery.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('.jqueryOptions').hide();
+
+  $('#choose').change(function () {
+    $('.jqueryOptions').slideUp();
+    $('.jqueryOptions').removeClass('current-opt');
+    $("." + $(this).val()).slideDown();
+    $("." + $(this).val()).addClass('current-opt');
+  });
+});
