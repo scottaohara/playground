83f0f9884fb91a6a9349206b53a5336870200fcc
Scott O'Hara <scottaohara@gmail.com>
2018-02-25 14:11:38 -0500

    data-ids for all
    little script to find all elements within a document and add data-ids to them

---
+++ JS_add_data-id_to_all/data-id-for-all.js
@@ -0,0 +1,36 @@
+;(function ( w, doc, undefined ) {
+	'use strict';
+
+	/**
+	 * Function to loop through all elements on
+	 * a page and supply each element with a [data-id]
+	 * attribute, with a unique value.
+	 */
+
+	var setDataID = function () {
+		var self;
+		var all = doc.querySelectorAll('*');
+		var i;
+
+		/**
+		 * Loop through all elements.
+		 * If an element already had a manually set data-id,
+		 * then just move on.
+		 * If no data-id exists, generate a random one.
+		 */
+		for ( i = 0; i < all.length; i++ ) {
+			self = all[i];
+
+			if ( self.hasAttribute('data-id') ) {
+				alert('move on');
+			}
+			else {
+				self.setAttribute('data-id', 'id_' + Math.floor(Math.random() * 999) + 1);
+			}
+		}
+	}
+
+	/* Go go JavaScript */
+	setDataID();
+
+})( window, document );
